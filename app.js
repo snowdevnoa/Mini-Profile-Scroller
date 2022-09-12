c5cf8b812c9ea2b24c4f32e7e7033df777cfaf75
xmasnoaa <xmas.noaa@gmail.com>
2022-09-12 12:52:26 -0500

    data example

---
+++ app.js
@@ -0,0 +1,28 @@
+//usually data will come from an api
+
+const data = [
+  {
+    name: 'John Doe',
+    age: 22,
+    gender: 'male',
+    lookingfor: 'female',
+    location: 'New York City NY',
+    image: 'https://randomuser.me/api/portraits/men/22.jpg',
+  },
+  {
+    name: 'Jen Smith',
+    age: 26,
+    gender: 'female',
+    lookingfor: 'male',
+    location: 'Miami FL',
+    image: 'https://randomuser.me/api/portraits/women/20.jpg',
+  },
+  {
+    name: 'Mary Williams',
+    age: 22,
+    gender: 'female',
+    lookingfor: 'male',
+    location: 'Austin TX',
+    image: 'https://randomuser.me/api/portraits/women/22.jpg',
+  },
+];
