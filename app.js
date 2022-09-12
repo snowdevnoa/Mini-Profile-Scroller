11f60158229150044ec59c9c3842f97eaa0b07b9
xmasnoaa <xmas.noaa@gmail.com>
2022-09-12 13:07:05 -0500

    profile iterator and display

--- app.js
+++ app.js
@@ -26,3 +26,46 @@ const data = [
     image: 'https://randomuser.me/api/portraits/women/22.jpg',
   },
 ];
+
+const profiles = profileIterator(data);
+
+//Call first profle
+nextProfile();
+
+//Next Event
+document.getElementById('next').addEventListener('click', nextProfile);
+
+function nextProfile() {
+  const currentProfile = profiles.next().value;
+
+  if (currentProfile !== undefined) {
+    document.getElementById('profileDisplay').innerHTML = `
+    <ul class="list-group">
+    <li class="list-group-item">Name: ${currentProfile.name}</li>
+    <li class="list-group-item">Age: ${currentProfile.age}</li>
+    <li class="list-group-item">Location: ${currentProfile.location}</li>
+    <li class="list-group-item">Preference: ${currentProfile.gender} looking for ${currentProfile.lookingfor}</li>
+
+    </ul>`;
+
+    document.getElementById(
+      'imageDisplay'
+    ).innerHTML = `<img src="${currentProfile.image}"/>`;
+  } else {
+    //no more profiles
+    window.location.reload();
+  }
+}
+
+//Profle Iterator
+
+function profileIterator(profiles) {
+  let nextIndex = 0;
+  return {
+    next: function () {
+      return nextIndex < profiles.length
+        ? { value: profiles[nextIndex++], done: false }
+        : { done: true };
+    },
+  };
+}
